fa3e4ae49a9af47967aff0ff007ee8686f799c2d
Googolaire <Thehuntermaxwell@gmail.com>
2023-09-29 10:46:05 -0500

    phonebook.c  added

---
+++ phonebook.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+/* 
+//Gather and Print Name Age Number
+int main(void)
+{
+    string name = get_string("What's your name? ");
+    int age = get_int("What's your age? ");
+    string number = get_string("What's your number? ");
+
+    printf("Age is %i. Name is %s. Phone Number is %s \n",age, name, number);
+}
+*/
+/*
+// Random For Loop structure 
+int main(void)
+{
+    for (int i = 0; i < 10; i++)
+    {
+        // code
+    }
+}
+*/
+/* 
+// Check for specific age using a Do While loop
+int main(void)
+{
+int age;
+do
+{
+    age = get_int("Age: ");
+}
+while (age < 15 || age > 20);
+} 
+*/
+int main(void)
+{
+int age;
+do
+{
+    age = get_int("Age: ");
+}
+while (age < 15 || age > 20);
+} 
