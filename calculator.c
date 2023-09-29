0982b24b261cfee7e105659448009832586b3a79
Googolaire <Thehuntermaxwell@gmail.com>
2023-09-28 23:16:55 -0500

    calculator.c added

---
+++ calculator.c
@@ -0,0 +1,21 @@
+#include <cs50.h>
+#include <stdio.h>
+
+/*
+int main(void)
+{
+    long x = get_long("x: ");
+    long y = get_long("y: ");
+
+    printf("%li\n", x + y);
+}
+*/
+
+int main(void)
+{
+    long x = get_long("x: ");
+    long y = get_long("y: ");
+
+    double z = (double) x / (double) y;
+    printf("%.20f\n", z);
+}
