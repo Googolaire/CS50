e2f81b1330528de115a8d637fd3c58842914d397
Googolaire <Thehuntermaxwell@gmail.com>
2023-10-07 11:02:20 -0500

    uppercase updated

--- uppercase.c
+++ uppercase.c
@@ -1,7 +1,9 @@
 #include <cs50.h>
 #include <stdio.h>
 #include <string.h>
-
+#include <ctype.h>
+// Calculating the upper and lower case manually using the differance in the asci chart
+/*
 int main(void)
 {
     string s = get_string("Before: ");
@@ -11,6 +13,25 @@ int main(void)
         if (s[i] >= 'a' && s[i] <= 'z')
         {
             printf("%c", s[i] - 32);
+        }
+        else
+        {
+            printf("%c", s[i]);
+        }
+    }
+    printf("\n");
+}
+*/
+// using the <ctype.h> library
+int main(void)
+{
+    string s = get_string("Before: ");
+    printf("After: ");
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (islower(s[i]))
+        {
+            printf("%c", toupper(s[i]));
         }
         else
         {
